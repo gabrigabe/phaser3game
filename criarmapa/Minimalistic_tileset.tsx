1acc2b66a32515927208b906ee2ad32e8c072d40
Gabriel Bezerra <gabrielbrodrigues1@gmail.com>
2021-09-27 20:07:09 -0300

    Hello World em Phaser

---
+++ criarmapa/Minimalistic_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Minimalistic_tileset" tilewidth="32" tileheight="32" tilecount="4" columns="2">
+ <image source="img/Minimalistic_tileset.png" trans="ff00ff" width="64" height="64"/>
+</tileset>
