659a066633acde8e14263081d894eb075ae38273
Gabriel Bezerra <gabrielbrodrigues1@gmail.com>
2021-10-18 18:20:51 -0300

    Reorganize file

--- criarmapa/pirates.v1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="pirates.v1" tilewidth="32" tileheight="32" tilecount="64" columns="8">
- <image source="img/pirates.v1.png" width="256" height="256"/>
-</tileset>
